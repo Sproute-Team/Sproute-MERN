67e1ba9da6e6a374ef36710392afad124a4a198e
Edmond GAKS <edmondgaks@gmail.com>
2022-05-19 09:05:12 +0200

    mergin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Landing from './Landing'
 import GetStarted from './Getstarted';
+import signup from './server';
+import getAllUsers from './getAllusers';
 
 function App() {
+  signup()
+  getAllUsers()
   return (
     <div className="App">
       <Landing />
